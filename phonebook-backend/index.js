f0711544eefa8d9faeda7234fb3e953021ae055d
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-07-08 18:25:43 +0300

    update 3.6

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,30 +4,59 @@ const app = express();
 app.use(express.json());
 
 let persons = [
-  { 
-    id: "1",
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: "2",
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: "3",
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: "4",
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
+  { id: "1", name: "Arto Hellas", number: "040-123456" },
+  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
 
+// GET all persons
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+// GET info
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const numPersons = persons.length;
+  res.send(`<p>Phonebook has info for ${numPersons} people</p><p>${date}</p>`);
+});
+
+// GET a single person
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// DELETE a person
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+});
+
+// POST a new person
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' });
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'name must be unique' });
+  }
+
+  const person = {
+    id: String(persons.length + 1), 
+    name: body.name,
+    number: body.number
+  };
+  persons = persons.concat(person);
+  res.json(person);
 });
 
 const PORT = 3001;
