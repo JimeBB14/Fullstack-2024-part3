79927ec223ea8a9b6bd14e519508e517a0e7233e
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-07-08 18:29:12 +0300

    update 3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
+
+// Morgan configuration for logging
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
 let persons = [
-  { id: "1", name: "Arto Hellas", number: "040-123456" },
-  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+  { id: '1', name: 'Arto Hellas', number: '040-123456' },
+  { id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: '3', name: 'Dan Abramov', number: '12-43-234345' },
+  { id: '4', name: 'Mary Poppendieck', number: '39-23-6423122' }
 ];
 
 // GET all persons
@@ -25,7 +42,7 @@ app.get('/info', (req, res) => {
 // GET a single person
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id;
-  const person = persons.find(p => p.id === id);
+  const person = persons.find((p) => p.id === id);
   if (person) {
     res.json(person);
   } else {
@@ -36,7 +53,7 @@ app.get('/api/persons/:id', (req, res) => {
 // DELETE a person
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id;
-  persons = persons.filter(p => p.id !== id);
+  persons = persons.filter((p) => p.id !== id);
   res.status(204).end();
 });
 
@@ -46,7 +63,7 @@ app.post('/api/persons', (req, res) => {
   if (!body.name || !body.number) {
     return res.status(400).json({ error: 'name or number missing' });
   }
-  if (persons.find(p => p.name === body.name)) {
+  if (persons.find((p) => p.name === body.name)) {
     return res.status(400).json({ error: 'name must be unique' });
   }
 
@@ -58,6 +75,12 @@ app.post('/api/persons', (req, res) => {
   persons = persons.concat(person);
   res.json(person);
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
